122603bffdf30cdd5531448b05090e276b067fa0
ArilexWang <383234436@qq.com>
2018-01-29 14:48:27 +0800

    fix

--- Test/urls.py
+++ Test/urls.py
@@ -20,5 +20,5 @@ from . import views
 from django.views.generic.base import RedirectView
 urlpatterns = [
     url(r'^$', views.hello),
-    url(r'^favicon.ico$',RedirectView.as_view(url=r'static/favicon.ico')),
+    url(r'^favicon\.ico$',RedirectView.as_view(url=r'static/favicon.ico',permanent=True)),
 ]
